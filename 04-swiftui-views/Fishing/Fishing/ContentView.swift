72ec1a2fb360a64f02b0d314ffc86f1f71c4130e
zzr9960004 <605142891@qq.com>
2021-07-12 22:50:41 +0800

    04-swiftui-views

---
+++ 04-swiftui-views/Fishing/Fishing/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  Fishing
+//
+//  Created by Zzr on 2021/7/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("🎣🎣🎣\n让鱼竿尽可能接近鱼")
+            Text("89米")
+            HStack {
+                Text("1米")
+                Slider(value: .constant(50), in: 1.0...100.0)
+                Text("100米")
+            }
+            Button(action: {}) {
+                Text("点我")
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+        ContentView()
+            .previewLayout(.fixed(width: 926, height: 428))
+    }
+}
