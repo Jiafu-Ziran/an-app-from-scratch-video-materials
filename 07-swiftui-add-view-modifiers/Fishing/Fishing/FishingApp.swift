c69da75f4ce602db06fbfa0e80e00ad7f2e02788
zzr9960004 <605142891@qq.com>
2021-07-24 11:20:33 +0800

    07-swiftui-add-view-modifiers

---
+++ 07-swiftui-add-view-modifiers/Fishing/Fishing/FishingApp.swift
@@ -0,0 +1,17 @@
+//
+//  FishingApp.swift
+//  Fishing
+//
+//  Created by Zzr on 2021/7/10.
+//
+
+import SwiftUI
+
+@main
+struct FishingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
