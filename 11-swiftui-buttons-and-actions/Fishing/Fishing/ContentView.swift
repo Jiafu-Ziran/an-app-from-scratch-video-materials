25cbab0cbd17f5cb1a010a14d834b6dba8160600
zzr9960004 <605142891@qq.com>
2021-07-30 17:34:56 +0800

    12-swiftui-state

---
+++ 11-swiftui-buttons-and-actions/Fishing/Fishing/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  Fishing
+//
+//  Created by Zzr on 2021/7/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("🎣🎣🎣\n让鱼竿尽可能接近鱼")
+                .bold()
+                .kerning(2.0)
+                .multilineTextAlignment(.center)
+                .lineSpacing(4.0)
+                .font(.footnote)
+            Text("89米")
+                .kerning(-1.0)
+                .font(.largeTitle)
+                .fontWeight(.black)
+            HStack {
+                Text("1米")
+                    .bold()
+                Slider(value: .constant(50), in: 1.0...100.0)
+                Text("100米")
+                    .bold()
+            }
+            Button(action: {
+                print("你好，SwiftUI")
+            }) {
+                Text("点我")
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+        ContentView()
+            .previewLayout(.fixed(width: 926, height: 428))
+    }
+}
---
+++ 12-swiftui-state/Fishing/Fishing/ContentView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  Fishing
+//
+//  Created by Zzr on 2021/7/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var alertIsVisible: Bool = false
+    
+    var body: some View {
+        VStack {
+            Text("🎣🎣🎣\n让鱼竿尽可能接近鱼")
+                .bold()
+                .kerning(2.0)
+                .multilineTextAlignment(.center)
+                .lineSpacing(4.0)
+                .font(.footnote)
+            Text("89米")
+                .kerning(-1.0)
+                .font(.largeTitle)
+                .fontWeight(.black)
+            HStack {
+                Text("1米")
+                    .bold()
+                Slider(value: .constant(50), in: 1.0...100.0)
+                Text("100米")
+                    .bold()
+            }
+            Button(action: {
+                print("你好，SwiftUI")
+                self.alertIsVisible = true
+            }) {
+                Text("点我")
+            }
+            .alert(isPresented: $alertIsVisible, content: {
+                return Alert(title: Text("你好呀！"), message: Text("这是我的第一个弹窗"), dismissButton: .default(Text("真棒！")))
+            })
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+        ContentView()
+            .previewLayout(.fixed(width: 926, height: 428))
+    }
+}
