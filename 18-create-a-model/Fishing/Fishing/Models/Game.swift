4b386cd96bd8506e29faec0c247dfa8d2eacccfd
zzr9960004 <605142891@qq.com>
2021-08-19 11:03:04 +0800

    18-create-a-model

---
+++ 18-create-a-model/Fishing/Fishing/Models/Game.swift
@@ -0,0 +1,18 @@
+//
+//  Game.swift
+//  Fishing
+//
+//  Created by Zzr on 2021/8/18.
+//
+
+import Foundation
+
+struct Game {
+    var target: Int = 42
+    var score: Int = 0
+    var round: Int = 1
+    
+    func points(sliderValue: Int) -> Int {
+        return 999
+    }
+}
